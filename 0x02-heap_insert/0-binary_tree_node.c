caf0ef3dd4c63411493c6ae93ee9e2aeb9644b8c
Jacob Ide <jacobmeide@gmail.com>
2020-12-19 20:21:01 +0000

    first attempt

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+#include <stdio.h>
+
+/**
+ * binary_tree_node
+ * @parent: Parent
+ * @value: Value
+ * Return: a pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	/* malloc and assign */
+	new = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	new->n = value;
+	new->parent = NULL;
+	new->left = NULL;
+	new->right = NULL;
+	/* If parent is null, start the tree */
+	if (parent == NULL)
+       	{
+		parent = new;
+		return (new);
+	}
+	else
+	{
+		new->parent = parent;
+		return (new);
+	}
+	return (NULL);
+}
