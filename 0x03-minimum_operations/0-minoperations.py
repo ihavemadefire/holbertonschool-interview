9b61e57769a2ed27c100fbd16f170f5bf8ea9aba
Jacob Ide <jacobmeide@gmail.com>
2021-01-08 10:23:56 -0600

    pep8 fix

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Docstring"""
 
+
 def minOperations(n):
     ret = 0
     for i in range(2, n):
-        while n%i == 0:
+        while n % i == 0:
             ret += i
-            n=n/i
+            n = n / i
     return ret
