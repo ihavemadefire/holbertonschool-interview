a31d93fcce100df9706f326ea4663441aeccef62
Jacob Ide <jacobmeide@gmail.com>
2021-01-17 19:01:34 +0000

    Initial attempt

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - check is a singly linked list is a palindrome
+ * @head: head of the linked list
+ * Return: Always 0.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	return (is_palindrome_rec(head, *head));
+}
+
+/**
+ * is_palindrome_rec - recursive call
+ * @left: double point to head of the linked list
+ * @right: pointer to right position
+ * Return: returns 0 if true, 1 if false
+ */
+
+int is_palindrome_rec(listint_t **left, listint_t *right)
+{
+	int i, j;
+
+	if (right == NULL)
+		return (1);
+
+	i = is_palindrome_rec(left, right->next);
+	if (i == 1)
+		return (1);
+
+	j = (right->n == (*left)->n);
+
+	*left = (*left)->next;
+
+	return (j);
+}
