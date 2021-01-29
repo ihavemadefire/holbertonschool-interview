3986e8415cc2588fc37c908b4ac24ad411049467
Jacob Ide <jacobmeide@gmail.com>
2021-01-29 12:47:17 -0600

    formating verification

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -18,7 +18,8 @@ counter = 0
 for line in fileinput.input():
     line_split = line.split()
     file_size = file_size + int(line_split[8])
-    stat_code[line_split[7]] = stat_code[line_split[7]] + 1
+    if line_split[7] in stat_code.keys():
+        stat_code[line_split[7]] = stat_code[line_split[7]] + 1
     counter += 1
     if counter == 10:
         counter = 0
