fbc647ad6dc0c6bd841e9aa887697d4ef9e0117e
Jacob Ide <jacobmeide@gmail.com>
2021-01-29 08:51:30 -0600

    add print conditionals

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -23,11 +23,19 @@ for line in fileinput.input():
     if counter == 9:
         counter = 0
         print("File size: {}".format(file_size))
-        print("200: {}".format(stat_code['200']))
-        print("301: {}".format(stat_code['301']))
-        print("400: {}".format(stat_code['400']))
-        print("401: {}".format(stat_code['401']))
-        print("403: {}".format(stat_code['403']))
-        print("404: {}".format(stat_code['404']))
-        print("405: {}".format(stat_code['405']))
-        print("500: {}".format(stat_code['500']))
+        if stat_code['200'] > 0:
+            print("200: {}".format(stat_code['200']))
+        if stat_code['301'] > 0:
+            print("301: {}".format(stat_code['301']))
+        if stat_code['400'] > 0:
+            print("400: {}".format(stat_code['400']))
+        if stat_code['401'] > 0:
+            print("401: {}".format(stat_code['401']))
+        if stat_code['403'] > 0:
+            print("403: {}".format(stat_code['403']))
+        if stat_code['404'] > 0:
+            print("404: {}".format(stat_code['404']))
+        if stat_code['405'] > 0:
+            print("405: {}".format(stat_code['405']))
+        if stat_code['500'] > 0:
+            print("500: {}".format(stat_code['500']))
