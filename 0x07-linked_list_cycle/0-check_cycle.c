6408f27507816055fb08411123c23930c6a521e7
Jacob Ide <jacobmeide@gmail.com>
2021-02-01 19:36:34 -0600

    Initial attempt

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -1,11 +1,23 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
  * check_cycle -
- *
+ * @list - Points to head node of list
  * Return: 
  */
 int check_cycle(listint_t *list)
 {
+    listint_t *tortoise = list;
+    listint_t *hare = list;
 
+    while (hare != NULL)
+    {
+        hare = hare->next->next;
+        tortoise = tortoise->next; 
+        if (hare == tortoise)
+            return (1);
+    }
+
+    return (0);
 }
