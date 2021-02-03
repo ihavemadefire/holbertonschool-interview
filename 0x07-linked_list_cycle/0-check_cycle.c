96498e37f97b3c810d8da4f0eb1d9ddb8b0f1f73
Jacob Ide <jacobmeide@gmail.com>
2021-02-03 08:44:26 -0600

    Remove segfault

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -10,8 +10,12 @@ int check_cycle(listint_t *list)
 {
     listint_t *tortoise = list;
     listint_t *hare = list;
+
+    if (list == NULL)
+      return (0);
     
-    while (hare->next->next != NULL)
+    
+    while (hare->next != NULL &&  hare->next->next != NULL)
     {
         hare = hare->next->next;
         tortoise = tortoise->next; 
