30faaf3416360d2e5c04e8aca6a3fab21b705a38
Jacob Ide <jacobmeide@gmail.com>
2021-02-03 08:09:05 -0600

    Add null check

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -11,6 +11,9 @@ int check_cycle(listint_t *list)
     listint_t *tortoise = list;
     listint_t *hare = list;
 
+    if (list == NULL)
+      return (0);
+    
     while (hare != NULL)
     {
         hare = hare->next->next;
