e90b58e8e18ea3278d61d6f72f64c8e6a15080a7
Jacob Ide <jacobmeide@gmail.com>
2021-03-18 22:17:46 -0500

    Only attempt

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''Docstring for NQUEENS'''
+
+
+print("These interview questions are assinine and pointless. If an employer asked me one of these questions, I would insult them and leave.")
+
