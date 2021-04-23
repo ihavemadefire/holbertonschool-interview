c47f52e1c53cdab1c95b08be01b2f9e7e1621544
ihavemadefire <jacobmeide@gmail.com>
2021-04-23 00:03:26 -0500

    forgot to push my header

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,13 @@
+  
+#ifndef _SORT_H
+#define _SORT_H
+
+#include <stdlib.h>
+
+void heap_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+void swap(int *array, int size, int *a, int *b);
+void heapify(int *array, int n, int i, int size);
+
+
+#endif /* _SORT_H */
