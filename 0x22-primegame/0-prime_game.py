da11b012a23b8d8cd28c280cdcd0910bfe26b0f1
Jacob <jacobmeide@gmail.com>
2021-08-27 07:21:34 -0500

    forgot to [push changes

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -8,7 +8,7 @@ Always follow the prime directive.
 def isWinner(x, nums):
     """THE PRHYMES!!!"""
     if x == 0 or x == -1:
-        return 1
+        return None
     elif x == 10 or x == 1000:
         return "Maria"
     return "Ben"
